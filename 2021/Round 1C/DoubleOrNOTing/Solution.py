7d59625b252aa09abfcf9ad082e15ba2ce07af74
gambler147 <zhengyi.fang95@gmail.com>
2021-05-09 19:18:18 -0400

    modified doubleornoting solution

--- 2021/Round 1C/DoubleOrNOTing/Solution.py
+++ 2021/Round 1C/DoubleOrNOTing/Solution.py
@@ -1,20 +1,4 @@
 import traceback
-def solve():
-  """
-  Group E and S by consecutive zeros and ones. The binary string can be then converted to a list of numbers
-  S = [a0, a1, ...] where a[i] is the number of bits in ith group, notice that the first group is always ones.
-  Lets assume converting S to E needs X NOT operations in total, and notice that each NOT operation removes the first
-  element in the group list and will not change the value in each group. It is obvious that X should not be exceeding
-  len(S) + 1. 
-
-  Also, a double operation will append a new group or update last group's bit count.
-
-  So we loop X from 0 to len(S)+1, then S will be converted to a new string S', if S' prime is prefix of E, then will need
-  to check how many additional groups in E's suffix, because adding an additional group requires 1 NOT operation and total 
-  number of additional groups should not exceed X, otherwise it is impossble.
-
-  reference: https://github.com/kamyu104/GoogleCodeJam-2021/blob/main/Round%201C/double_or_noting.py
-  """
 
 def flip(s):
     return "".join(["01"[c == '0'] for c in s]).lstrip('0') or "0"
@@ -27,6 +11,21 @@ def not_count(s):
     return ans
 
 def solve():
+    """
+    Group E and S by consecutive zeros and ones. The binary string can be then converted to a list of numbers
+    S = [a0, a1, ...] where a[i] is the number of bits in ith group, notice that the first group is always ones.
+    Lets assume converting S to E needs X NOT operations in total, and notice that each NOT operation removes the first
+    element in the group list and will not change the value in each group. It is obvious that X should not be exceeding
+    len(S) + 1. 
+
+    Also, a double operation will append a new group or update last group's bit count.
+
+    So we loop X from 0 to len(S)+1, then S will be converted to a new string S', if S' prime is prefix of E, then will need
+    to check how many additional groups in E's suffix, because adding an additional group requires 1 NOT operation and total 
+    number of additional groups should not exceed X, otherwise it is impossble.
+
+    reference: https://github.com/kamyu104/GoogleCodeJam-2021/blob/main/Round%201C/double_or_noting.py
+    """
     S, E = input().strip().split()
     # loop through all possible number of flips
     result = float('inf')
